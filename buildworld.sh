208becf349af953c01f94e1a4224d9bf3b8d835d
hadojae <jae.williams@gmail.com>
2014-12-09 23:15:25 -0600

    moloch vars, moloch config

--- buildworld.sh
+++ buildworld.sh
@@ -1,24 +1,64 @@
 #!/bin/sh
+
+#GLOBALS
+oinkcode=""				#Enter in oinkcode to use ETPRO
+user="cuckoo"				#Needs to be cuckoo for now
+sniff_sub="192.168.56.0/24"		#Subnet that suricata should treat as internal
+sniff_net="vboxnet0"			#Network that your vm traffic is going over
+out_int="eth0"				#Outbound interface of VM Host
+
+### Moloch Config Variables ###
+es_mem="10G"                                    # How much memory should elasticsearch for moloch have? (< 32GB)
+moloch_password="OmG_37_Rul3z!1!"               # Password for Moloch
+moloch_fqdn="etcuckoo.localhost"                # FQDN for Moloch Cert
+moloch_country="INTERNET"                               # Country for Moloch Cert
+moloch_state="WIN"                                 # State for Moloch Cert
+moloch_orgname="DOoMP"                               # Org Name for Moloch Cert
+moloch_orgunit="MISCREANT PUNCHERS"                               # Org unit for Moloch Cert
+moloch_locality="EARTH"                              # Locality for Moloch Cert
+
+#check for cuckoo user
+if [ `whoami` != $user ]; then
+ echo "Please create and run this script as the user 'cuckoo'."
+ exit 0
+fi
+
+#setup oinkcode
+if ["$oinkcode" = ""]; then
+ rule_url="https://rules.emergingthreatspro.com/|emerging.rules.tar.gz|open"
+else
+ rule_url="https://rules.emergingthreatspro.com/|etpro.rules.tar.gz|$oinkcode"
+fi
+
+#Update Ubuntu
 sudo apt-get update -y && sudo apt-get upgrade -y
+
+#Deps
 sudo apt-get install -y vim screen unzip python python-dpkt python-jinja2 python-magic python-pymongo python-gridfs python-libvirt python-bottle python-chardet tcpdump clamav-daemon clamav-unofficial-sigs clamav clamav-base libcap2-bin python-dev build-essential subversion pcregrep libpcre++-dev python-pip ssdeep libfuzzy-dev git automake libtool autoconf libapr1 libapr1-dev libnspr4-dev libnss3-dev libwww-Perl libcrypt-ssleay-perl python-dev python-scapy python-yaml bison libpcre3-dev bison flex libdumbnet-dev autotools-dev libnet1-dev libpcap-dev libyaml-dev libnetfilter-queue-dev libprelude-dev zlib1g-dev libz-dev libcap-ng-dev libmagic-dev python-mysqldb lua-zip-dev lua-zip luarocks cmake libjansson-dev libswitch-perl libcdio-utils mongodb-server python-simplejson p7zip-full 
 
+#Setup tcpdump
 sudo setcap cap_net_raw,cap_net_admin=eip /usr/sbin/tcpdump
+
+#Pip install deps
 sudo pip install bottle django==1.6.7 pycrypto clamd
 
-#wget https://pefile.googlecode.com/files/pefile-1.2.10-139.tar.gz
+#Install PEfile 
+#src:https://pefile.googlecode.com/files/pefile-1.2.10-139.tar.gz
 tar -xzvf pefile-1.2.10-139.tar.gz
 cd pefile-1.2.10-139
 python setup.py build
 sudo python setup.py install
 
-#wget https://distorm.googlecode.com/files/distorm3.zip
+#Install Distrom3 
+#src:https://distorm.googlecode.com/files/distorm3.zip
 unzip distorm3.zip
 cd distorm3
 python setup.py build
 sudo python setup.py install
 cd ..
 
-#wget https://github.com/plusvic/yara/archive/2.1.0.tar.gz
+#Install Yara 2.1.0 
+#src:https://github.com/plusvic/yara/archive/2.1.0.tar.gz
 tar -zxf v3.1.0.tar.gz
 cd yara-3.1.0
 ./bootstrap.sh
@@ -116,12 +156,6 @@ cd ..
 
 sudo git clone https://github.com/EmergingThreats/et-luajit-scripts /usr/local/suricata/et-luajit-scripts
 sudo cp /usr/local/suricata/et-luajit-scripts/* /usr/local/suricata/etc/
-read -p  "Enter your ETPRO oinkcode if you have one [ENTER]: " oinkcode
-if ["$oinkcode" = ""]; then
- rule_url="https://rules.emergingthreatspro.com/|emerging.rules.tar.gz|open"
-else
- rule_url="https://rules.emergingthreatspro.com/|etpro.rules.tar.gz|$oinkcode"
-fi
 
 echo "rule_url=$rule_url
 ignore=local.rules
@@ -156,13 +190,27 @@ ruleupdates.sh
 
 unzip moloch-master.zip
 cd moloch-master
+cp ../easybutton-config.sh .
 patch -p1 < ../moloch-geoip-fix.diff
 patch -p1 < ../moloch-free-space.diff
+sed -i 's/echo -n "Use pfring?.*$/USEPFRING=no/' easybutton-singlehost.sh
+sed -i 's/read USEPFRING//' easybutton-singlehost.sh
+sed -i 's/echo -n "Memory to give to elasticsearch, box MUST have more then this available: [512M] "//' easybutton-singlehost.sh
+sed -i "s,read ESMEM,ESMEM=\"$es_mem\"," easybutton-singlehost.sh
+sed -i "s,USERNAME=CHANGEME,USERNAME=cuckoo," easybutton-config.sh
+sed -i "s,GROUPNAME=CHANGEME,GROUPNAME=cuckoo," easybutton-config.sh
+sed -i "s,PASSWORD=CHANGEME,PASSWORD=${moloch_password}," easybutton-config.sh
+sed -i "s,INTERFACE=CHANGEME,INTERFACE=not_needed," easybutton-config.sh
+sed -i "s,FQDN=CHANGEME,FQDN=${moloch_fqdn}," easybutton-config.sh
+sed -i "s,COUNTRY=CHANGEME,COUNTRY=${moloch_country}," easybutton-config.sh
+sed -i "s,STATE=CHANGEME,STATE=${moloch_state}," easybutton-config.sh
+sed -i "s,ORG_NAME=CHANGEME,ORG_NAME=${moloch_orgname}," easybutton-config.sh
+sed -i "s,ORG_UNIT=CHANGEME,ORG_UNIT=${moloch_orgunit}," easybutton-config.sh
+sed -i "s,LOCALITY=CHANGEME,LOCALITY=${moloch_locality}," easybutton-config.sh
 sudo ./easybutton-singlehost.sh
 cd ..
 sudo pkill -f "/data/moloch/bin/node viewer.js"
 sudo pkill -f "/data/moloch/elasticsearch-0"
-
 
 sudo git clone https://github.com/EmergingThreats/cuckoo-1.1.git /data/cuckoo
 
@@ -183,8 +231,13 @@ chmod +x services/*
 sudo cp services/* /etc/init.d/
 sudo update-rc.d suricata defaults
 
+#add stuff to rc.local
 echo "etc/init.d/moloch start
 /etc/init.d/cuckoo start
+iptables -A FORWARD -o $out_int -i $sniff_net -s $sniff_sub -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A POSTROUTING -t nat -j MASQUERADE
+sysctl -w net.ipv4.ip_forward=1
 exit 0" | sudo tee /etc/rc.local
 
 CURRENT_USER=`whoami`
@@ -203,10 +256,16 @@ sudo apt-get install virtualbox-4.3 -y
 echo xfce4-session > ~/.xsession
 sudo service xrdp restart
 
-sudo virsh net-destroy default
-sudo virsh net-undefine default
-sudo service libvirtd restart
+#sudo virsh net-destroy default
+#sudo virsh net-undefine default
+#sudo service libvirtd restart
 
 echo "#!/bin/sh
 su cuckoo -c \"/usr/local/bin/ruleupdates.sh\" && /etc/init.d/suricata restart" | sudo tee /etc/cron.daily/ruleupdates
 sudo chmod +x /etc/cron.daily/ruleupdates
+
+#Configure Iptables to allow networking to work
+iptables -A FORWARD -o $out_int -i $sniff_net -s $sniff_sub -m conntrack --ctstate NEW -j ACCEPT
+iptables -A FORWARD -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A POSTROUTING -t nat -j MASQUERADE
+sysctl -w net.ipv4.ip_forward=1
---
+++ easybutton-config.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This script fills in the values in the etc/*.template files.
+# This script is auto run by easybutton-singlehost.sh
+
+TDIR=CHANGEME
+if [ "$#" -gt 0 ]; then
+    TDIR=$1
+fi
+
+USERNAME=CHANGEME
+GROUPNAME=CHANGEME
+PASSWORD=CHANGEME
+INTERFACE=CHANGEME
+FQDN=CHANGEME
+COUNTRY=CHANGEME
+STATE=CHANGEME
+ORG_NAME=CHANGEME
+ORG_UNIT=CHANGEME
+LOCALITY=CHANGEME
+
+cat ${TDIR}/etc/openssl.cnf.template | sed -e 's/_ORGANIZATION_NAME_/'${ORG_NAME}'/g' -e 's/_COMMON_NAME_/'${FQDN}'/g' -e 's/_COUNTRY_/'${COUNTRY}'/g' -e 's/_STATE_OR_PROVINCE_/'${STATE}'/g' -e 's/_LOCALITY_/'${LOCALITY}'/g' -e 's/_ORGANIZATION_UNIT_/'${ORG_UNIT}'/g'  > ${TDIR}/etc/openssl.cnf
+
+cat ${TDIR}/etc/config.ini.template | sed -e 's/_PASSWORD_/'${PASSWORD}'/g' -e 's/_USERNAME_/'${USERNAME}'/g' -e 's/_GROUPNAME_/'${GROUPNAME}'/g' -e 's/_INTERFACE_/'${INTERFACE}'/g'  -e "s,_TDIR_,${TDIR},g" > ${TDIR}/etc/config.ini
+
+cd ${TDIR}/etc/
+openssl req -new -newkey rsa:2048 -nodes -keyout moloch.key -out moloch.csr -config ${TDIR}/etc/openssl.cnf
+openssl x509 -req -days 3650 -in moloch.csr -signkey moloch.key -out moloch.crt
